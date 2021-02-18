46ed017cd1ae520fc4b4a35660b1b0f53d2b6ba9
Lupino <patrick@lupino.org>
2021-02-18 14:52:34 +0100

    klammern escaped

--- index.sh
+++ index.sh
@@ -37,15 +37,15 @@ fi
 sed -i -e 's/"|/\\\//g' $1.$idx
 sed -i -e 's/"=/-/g' $1.$idx
 sed -i -e 's/"~/-/g' $1.$idx
-sed -i -e 's/"|hyperindexformat{\\\([^}]\)/\\\/\1/g' $1.$idx
+sed -i -e 's/"|hyperindexformat\{\\\([^\}]\)/\\\/\1/g' $1.$idx
 if egrep '[^\\]"' $1.$idx >/dev/null ; then
   echo "Unescaptes Gänsefüßchen in \"$1.idx\" gefunden. Bitte escapen oder ersetzen, denn xindy schluckt unescapte Gänsefüßchen."
   exit 1
 fi
 
-perl -i -p -0 -e 's/hyperindexformat{\\see ?({[^}]+)}/see$1/g' $1.$idx
-perl -i -p -0 -e 's/hyperindexformat{\\seealso ?({[^}]+)}/seealso$1/g' $1.$idx
-perl -i -p -0 -e 's/(\\indexentry ?{.+)(\|seealso{.+})}{(.+)}/$1}{$3}\n$1$2}{$3}/g' $1.$idx #seealso-Eintrag verdoppeln, um Seitenzahl vor texindy zu retten
+perl -i -p -0 -e 's/hyperindexformat\{\\see ?(\{[^\}]+)\}/see$1/g' $1.$idx
+perl -i -p -0 -e 's/hyperindexformat\{\\seealso ?(\{[^\}]+)\}/seealso$1/g' $1.$idx
+perl -i -p -0 -e 's/(\\indexentry ?\{.+)(\|seealso\{.+\})\}\{(.+)\}/$1}{$3}\n$1$2}{$3}/g' $1.$idx #seealso-Eintrag verdoppeln, um Seitenzahl vor texindy zu retten
 perl -i -p -0 -e 's/\(hyperpage/\(/g' $1.$idx
 /usr/local/texlive/2019/bin/x86_64-linux/xindy -v -d script -M transpect.xdy -L $lang -C utf8 -M tex/inputenc/utf8 -M german-sty.xdy -M texindy -M page-ranges -M word-order -M german-sty.xdy -I latex -d level=3 -t xindy.log $1.$idx -o $1.$ind
 perl -i -p -0 -e 's/(item[^\n]+)(\\enskip [0-9]{1,2}\n)/$1\\nobreak$2/g' $1.$ind
@@ -60,7 +60,7 @@ perl -i -p -0 -e 's/(\n {6,6}\\subsubitem[^\n]+\n {4,4}\\subitem)/\\nopagebreak$
 perl -i -p -0 -e 's/(\n {6,6}\\subsubitem[^\n]+\n\n {2,2}\\indexspace)/\\nopagebreak$1/g' $1.$ind              #kein Unteruntereintrag-Hurenkind
 
 if [ $dofl ]; then
-    perl -i -p -0 -e "s/lettergroup{([^}]+)}/lettergroup{\\\\foreignlanguage{$lang}{\$1}}/g" $1.$ind
+    perl -i -p -0 -e "s/lettergroup\{([^\}]+)\}/lettergroup{\\\\foreignlanguage{$lang}{\$1}}/g" $1.$ind
 fi
 
 # Aufruf zum Überschreiben der texindy-spezifischen Unsichtbarmachung von LaTeX-Makros
