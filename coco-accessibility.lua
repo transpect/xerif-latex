abe58c644b562682695b3d31c8f70ed63382e0fd
Lupino <patrick@lupino.org>
2023-12-20 12:44:33 +0100

    meta data now PDF/A-3u conform

--- coco-accessibility.lua
+++ coco-accessibility.lua
@@ -12,20 +12,23 @@ local function extract_meta()
   local content = f:read("*all")
   f:close()
   if (content:find('<dc:title>')) then
-    meta.Title = content:gsub('.*<dc:title><rdf:Alt><rdf:li[^>]*>(.*)</rdf:li></rdf:Alt></dc:title>.*', "%1")
-    log(">>>" .. meta.Title)
+    meta.Title = content:gsub('.*<dc:title>[^<]*<rdf:Alt>[^<]*<rdf:li[^>]*>(.*)</rdf:li>[^<]*</rdf:Alt>[^<]*</dc:title>.*', "%1")
+    -- log(">>>" .. meta.Title)
   end
   local authors
   local author = {}
   if (content:find('<dc:creator>')) then
-    authors = content:gsub('.*<dc:creator><rdf:Seq>(.*)</rdf:Seq></dc:creator>.*', "%1")
+    authors = content:gsub('.*<dc:creator>[^<]*<rdf:Seq>(.*)</rdf:Seq>[^<]*</dc:creator>.*', "%1")
     for k in string.gmatch(authors, "<rdf:li>([^>]+)</rdf:li>") do
       table.insert(author , k)
     end
     meta.Author = table.concat(author, ', ')
   end
-  log(">>>>>>>>\nTitel: " .. meta.Title .. "\nAuthors: " ..  meta.Author .. "\n<<<<<<<<")
 end
-cocotex = {}
-cocotex.extract_meta = extract_meta
-return cocotex
+if type(cocotex) ~= 'table' then
+  cocotex = {}
+end
+cocotex.ally = {}
+cocotex.ally.extract_meta = extract_meta
+cocotex.ally.meta = meta
+return cocotex.ally
