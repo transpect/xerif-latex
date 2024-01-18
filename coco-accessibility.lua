73440523dc2b089dfc95e37403f5d4f479a9d22a
Lupino <patrick@lupino.org>
2024-01-18 16:59:34 +0100

    ADDED: Interfaces for output intent and and PDF standard conformance

--- coco-accessibility.lua
+++ coco-accessibility.lua
@@ -4,31 +4,31 @@ local meta = {
   Title = '',
   Creator = '',
   Producer = '',
-  Keywords = ''
-}
-local function extract_meta()
-  local xmpfile = ltpdfa.metadata.xmphandler.fromFile(ltpdfa.config.metadata.xmpfile)
-  local f = io.open(xmpfile, "r")
-  local content = f:read("*all")
-  f:close()
-  if (content:find('<dc:title>')) then
-    meta.Title = content:gsub('.*<dc:title>[^<]*<rdf:Alt>[^<]*<rdf:li[^>]*>(.*)</rdf:li>[^<]*</rdf:Alt>[^<]*</dc:title>.*', "%1")
-    -- log(">>>" .. meta.Title)
-  end
-  local authors
-  local author = {}
-  if (content:find('<dc:creator>')) then
-    authors = content:gsub('.*<dc:creator>[^<]*<rdf:Seq>(.*)</rdf:Seq>[^<]*</dc:creator>.*', "%1")
-    for k in string.gmatch(authors, "<rdf:li>([^>]+)</rdf:li>") do
-      table.insert(author , k)
+  Keywords = '',
+  extract = function ()
+    local xmpfile = ltpdfa.metadata.xmphandler.fromFile(ltpdfa.config.metadata.xmpfile)
+    local f = io.open(xmpfile, "r")
+    local content = f:read("*all")
+    f:close()
+    if (content:find('<dc:title>')) then
+      Title = content:gsub('.*<dc:title>[^<]*<rdf:Alt>[^<]*<rdf:li[^>]*>(.*)</rdf:li>[^<]*</rdf:Alt>[^<]*</dc:title>.*', "%1")
+      -- log(">>>" .. meta.Title)
     end
-    meta.Author = table.concat(author, ', ')
+    local authors
+    local author = {}
+    if (content:find('<dc:creator>')) then
+      authors = content:gsub('.*<dc:creator>[^<]*<rdf:Seq>(.*)</rdf:Seq>[^<]*</dc:creator>.*', "%1")
+      for k in string.gmatch(authors, "<rdf:li>([^>]+)</rdf:li>") do
+        table.insert(author , k)
+      end
+      Author = table.concat(author, ', ')
+    end
   end
-end
+}
 if type(cocotex) ~= 'table' then
   cocotex = {}
 end
-cocotex.ally = {}
-cocotex.ally.extract_meta = extract_meta
-cocotex.ally.meta = meta
+cocotex.ally = {
+  meta = meta
+}
 return cocotex.ally
